b95a0534d7403eddf800a7eba0e521eb47935ab5
BattiestWinter <spartanc932@gmail.com>
2022-03-23 16:13:34 -0600

    Componente - Informacion

---
+++ vuex-pastelite/src/components/Informacion.vue
@@ -0,0 +1,19 @@
+<template>
+    <!--Pie de página-->
+    <footer class="w-100 d-flex align-items justify-content-center">
+        <p class="px-2 pt-2">Dirección: Nueva calle Inventada número 105</p>
+        <p class="px-2 pt-2">Teléfono: 5535316490</p>
+        <p class="px-2 pt-2">Horario de atención: Lunes a Sábado de 9:00 a 19:00 horas</p>
+        <div id="iconos">
+            <a href=""><i class="bi bi-facebook"></i></a>
+            <a href=""><i class="bi bi-twitter"></i></a>
+            <a href=""><i class="bi bi-instagram"></i></a>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Informacion'
+}
+</script>
---
+++ vuex-pastelite/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Informacion/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Informacion from '@/components/Informacion.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    Informacion
+  }
+}
+</script>
